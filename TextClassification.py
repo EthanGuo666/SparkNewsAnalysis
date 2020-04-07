548cdd71698ece67b892dcc8a075dd945621984b
Yixuan Guo <gyixuan@vmware.com>
2020-04-07 22:21:52 +0800

    Updated the latest version of classification function.

--- TextClassification.py
+++ TextClassification.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 # Read data
 path = "data/origin_news_data.csv"
-data = pd.read_csv(path, encoding = 'GBK')
+data = pd.read_csv(path, encoding='GBK')
 
 # index是表格左边那一排纵向的0，到100...
 # 横行数量                  # 纵列数量                   # 取表格的某整列
-#print(data.shape[0])      #print(data.shape[1])       # #print(data.link)
+# print(data.shape[0])      #print(data.shape[1])       # #print(data.link)
 
 # 中国科协
 national_assoc = 0
@@ -15,21 +15,17 @@ local_assoc = 1
 # 全国学会
 national_academy = 2
 
-# 互斥量，每条新闻只能改一次属性
-mutex = 0
 def addtype():
-    # 遍历整张表, 也可以用
-    # for row in data.itertuples(index=True, name='Pandas'):
-    #     print(getattr(row, "title"))
-    for index, row in data.iterrows():
-        # print("data.iloc[index].type:",data.iloc[index].type)
-        text = data.iloc[index].title + " " + data.iloc[index].main_body
-        if "中国科协" in text:
-            data.iloc[index].type = 0
-            print("8\n8\n8\n8\n8\n8\n8\n")
-        if ("省科协" in text) or ("市科协" in row.title):
-            data.iloc[index].type = 1
-        if "学会" in text:
-            data.iloc[index].type = 2
-        print("data.iloc[index]:", data.iloc[index])
+    # 遍历整张表
+    for i in range(len(data)):
+        text = str(data.loc[[i], ['title']]) + " " + str(data.loc[[i], ['main_body']])
+        if ("省科" in text) or ("市科" in text) or ("省政府" in text) or ("市政府" in text) or ("基层" in text) or ("区" in text):
+            data.loc[[i], ['type']] = local_assoc
+        elif "学会" in text:
+            data.loc[[i], ['type']] = national_academy
+        else:
+            data.loc[[i],['type']] = national_assoc
+    # print(data)
+
 addtype()
+print(data)
